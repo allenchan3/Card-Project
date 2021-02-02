0768f8f76f1a44823ca35185797aab710184f621
Allen Chan <AllenChan3@gmail.com>
2021-02-02 17:02:47 -0500

    update of homepage menu in html

--- cardproject/pages/views.py
+++ cardproject/pages/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-def home(reqest):
+def home(request):
     return render(request, 'pages/home.html')
 
 def inventory(request):
