cffc0d2c8e5c63ebb85a037c6819e7b4055fd422
Allen Chan <AllenChan3@gmail.com>
2021-02-06 00:00:55 -0500

    query all cards in view, used context and templating to push data to html

--- cardproject/pages/views.py
+++ cardproject/pages/views.py
@@ -1,10 +1,19 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from pages.models import *
+
 
 def home(request):
     return render(request, 'pages/home.html')
 
 def list_all_cards(request):
-    return HttpResponse('<h1>Test </h1>')
+    
+    all_cards = Card.objects.all()
+    print(all_cards)    
+    
+    context= {
+        "cards":all_cards,
+    }
+    return render(request,'pages/inventory.html', context)
 
 def stats(request):
     return render(request, 'pages/stats.html')
